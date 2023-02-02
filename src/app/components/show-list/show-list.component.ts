34aad6aa802cdf2bcbccc7eb957e8b19dec3b442
Bayern2021 <90843232+Bayern2021@users.noreply.github.com>
2023-02-02 09:39:44 +0100

    2 files modified

--- src/app/components/show-list/show-list.component.ts
+++ src/app/components/show-list/show-list.component.ts
@@ -8,12 +8,15 @@ import { ShowDataService} from '../../services/show-data.service';
   styleUrls: ['./show-list.component.css']
 })
 export class ShowListComponent implements OnInit {
-  
+  editShow :  Show ;
   constructor(private showDataService: ShowDataService){
     
   } 
   get shows(): Show[] {
     return this.showDataService.shows;
+  }
+  edit(show : Show) {
+   this.editShow = show;
   }
   
   
